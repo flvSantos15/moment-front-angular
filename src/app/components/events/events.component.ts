ec8a859019d0e31c8072ef74bd84f1b42084685a
flvSantos15 <flvSantos300@gmail.com>
2023-02-27 21:34:48 -0300

    feat: change-number + emitter + events

---
+++ src/app/components/events/events.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css'],
+})
+export class EventsComponent {
+  show: boolean = false;
+
+  showMessage(): void {
+    this.show = !this.show;
+  }
+
+  overMessage(): void {
+    alert('Over message');
+  }
+}
